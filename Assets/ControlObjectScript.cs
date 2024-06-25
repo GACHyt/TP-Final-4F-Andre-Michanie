ee05804479464c049893211784e742c6a2bf1b9c
GACHyt <146768965+GACHyt@users.noreply.github.com>
2024-06-25 13:59:09 -0300

    Initial commit

---
+++ Assets/ControlObjectScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlObjectScript : MonoBehaviour
+{
+    public GameObject[] objetos;
+    int currentIndex = -1;
+    void Start()
+    {
+        DesactivarTodas();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentIndex++;
+            if (currentIndex >= objetos.Length)
+            {
+                currentIndex = 0;
+            }
+            DesactivarTodas();
+            Activar(currentIndex);
+        }
+    }
+    void DesactivarTodas()
+    {
+        for (int i = 0; i < objetos.Length; i++)
+        {
+            objetos[i].SetActive(false);
+        }
+    }
+
+    void Activar(int i)
+    {
+        objetos[i].SetActive(true);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TP-Final-4F-Andre-Michanie
+ 
